2af7e98f82a931e6d671917af33efdf5ef62b251
Daanta <daanta@naver.com>
2022-03-09 23:55:11 +0900

    220309_2354 MainSystem.class를 싱글톤 인스턴스로 불러오도록 설정변경

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -13,23 +13,28 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 class Main extends ListenerAdapter {
 
 	// 어디서든 호출할 수 있는 JDA 생성
-    public static JDA jda_obj = null;
+	public static JDA jda_obj = null;
+
+	// 싱글톤 인스턴스 획득
+	public static MainSystem mainSystem = MainSystem.getInstance();
+
 
 	// 명령어에 따른 동작 실행
-    @Override
-    public void onMessageReceived(MessageReceivedEvent event) { Cmd.command(event); }
+	@Override
+	public void onMessageReceived(MessageReceivedEvent event) { Cmd.command(event); }
 
 	// 서버의 실행
 	public static void main(String[] args) throws Exception {
 
 		// 환경설정 확인
 		$.pn("[[[잔디체커 환경설정 확인]]]");
-		$.pf(" - 실행 경로 확인: %s\n", MainSystem.PATH);
-		$.pf(" - 토큰: %s\n", MainSystem.TOKEN);
+		$.pf(" - 실행 경로 확인: %s\n", mainSystem.getPath());
+		$.pf(" - 토큰: %s\n", mainSystem.getToken());
 		$.pn(" - 명단 확인: ");
-		for(int i = 0; i < MainSystem.LIST.length; i++) {
-			String[] s = MainSystem.LIST[i];
-			$.pf("    ㄴ %d번째 인원: '%s' (Github ID: %s)", s[0], s[1]);
+		String[][] members = mainSystem.getMembers();
+		for(int i = 0; i < members.length; i++) {
+			String[] s = members[i];
+			$.pf("	ㄴ %d번째 인원: '%s' (Github ID: %s)", s[0], s[1]);
 		}
 
 		// 인스턴스 잡기
@@ -37,7 +42,7 @@ class Main extends ListenerAdapter {
 		JDA jda = JdaObj.instance;
 		// 기본 jda를 만든다
 		try {
-			jda = JDABuilder.createDefault(MainSystem.TOKEN).build(); // 봇을 만들어 로그인시킨 뒤, JdaObj의 인스턴스 값으로 할당
+			jda = JDABuilder.createDefault(mainSystem.token).build(); // 봇을 만들어 로그인시킨 뒤, JdaObj의 인스턴스 값으로 할당
 			$.pn("JDA 인스턴스 생성 완료:" + jda.toString() );
 		} catch (LoginException e) { e.printStackTrace(); }
 
@@ -50,6 +55,6 @@ class Main extends ListenerAdapter {
 		// 모든 실행 준비가 끝났다.
 		$.pn("[[[잔디체커 실행 완료]]]");
 
-    }
+	}
 
 }
