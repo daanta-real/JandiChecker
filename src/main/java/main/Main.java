d161e50740c25c043a67bd812c5c9670cc691b96
Daanta <daanta@naver.com>
2022-03-11 23:10:47 +0900

    220311_2310 초기 로딩부분 각 모듈로 분리

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -12,31 +12,20 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 class Main extends ListenerAdapter {
 
-	// 어디서든 호출할 수 있는 JDA 생성
-	public static JDA jda_obj = null;
-
-	// 싱글톤 인스턴스 획득
+	// 인스턴스
+	// 어디서든 호출할 수 있는 JDA 인스턴스
+	public static JDA jda;
+	// 싱글톤 인스턴스
 	public static MainSystem mainSystem;
 
-
-	// 명령어에 따른 동작 실행
-	@Override
-	public void onMessageReceived(MessageReceivedEvent event) { Cmd.command(event); }
-
-	// 서버의 실행
-	public static void main(String[] args) throws Exception {
-
-		// 환경설정 확인
-		$.pn("[[[잔디체커 시작]]]\n");
+	// 환경설정 로드
+	public void readySystem() {
 
 		// mainSystem 변수의 인스턴스 설정
 		mainSystem = MainSystem.getInstance();
 
 		// 상세 환경변수 확인 시작
-		$.pn("[[[잔디체커 환경설정 확인]]]");
 		$.pr(" - 메인 설정 확인: ");
-		$.pn(mainSystem);
-
 		$.pf(" - 실행 경로 확인: %s\n", mainSystem.getPath());
 		$.pf(" - 토큰: %s\n", mainSystem.getToken());
 		$.pn(" - 명단 확인: ");
@@ -47,9 +36,11 @@ class Main extends ListenerAdapter {
 			$.pf("    %c─ %d번째 인원: '%s' (Github ID: %s)\n", line, i, s[0], s[1]);
 		}
 
-		// 인스턴스 잡기
+	}
+	public void readyJda() {
+		// JDA 인스턴스 잡기
 		$.pn("\n[[[잔디체커 JDA 인스턴스 생성]]]");
-		JDA jda = JdaObj.instance;
+		jda = JdaObj.instance;
 		// 기본 jda를 만든다
 		try {
 			jda = JDABuilder.createDefault(mainSystem.token).build(); // 봇을 만들어 로그인시킨 뒤, JdaObj의 인스턴스 값으로 할당
@@ -61,9 +52,42 @@ class Main extends ListenerAdapter {
 		ListenerAdapter bot = new Main(); // 리스너 봇
 		jda.addEventListener(bot); // 만들어진 리스너 봇을 JdaObj의 인스턴스 내부에 할당
 		$.pn(" - 이벤트 리스너 생성: " + bot.toString());
+	}
+	public void ready() {
+
+		// 환경설정 확인
+		$.pn("[[[잔디체커 시작]]]");
+
+		// 환경설정 로드
+		$.pn("\n[[[잔디체커 환경설정 로드]]]");
+		readySystem();
+
+		// JDA 로드
+		$.pn("\n[[[잔디체커 JDA 로드]]]");
+		readyJda();
 
 		// 모든 실행 준비가 끝났다.
 		$.pn("\n[[[잔디체커 실행 완료]]]");
+
+	}
+
+	// 초기화 이후, 명령어에 따른 동작 실행
+	@Override
+	public void onMessageReceived(MessageReceivedEvent event) {
+		String id = event.getChannel().getId();
+		$.pn("[[명령어 입력정보 시작]]");
+		$.pn(" - 채널 ID: " + id);
+		$.pn("[[명령어 입력정보 끝]]");
+		Cmd.command(event);
+	}
+
+	// 서버의 실행
+	public static void main(String[] args) throws Exception {
+
+		/*TextChannel textChannel = YourBotInstance.getJda().getTextChannelById("386242731875368960");
+		if(textChannel.canTalk()) {
+		    textChannel.sendMessage("Your message here.").queue();
+		}*/
 
 	}
 
