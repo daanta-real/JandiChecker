f3d042f4c143f3c3c77872586536e91f78b6eef2
Junsung Park / Daanta <daanta@naver.com>
2023-01-09 11:15:34 +0900

    230109_1115 Fixed msg overflow problem (>2000)

--- src/main/java/jda/JdaMsgSender.java
+++ src/main/java/jda/JdaMsgSender.java
@@ -4,6 +4,7 @@ package jda;
 import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import org.apache.commons.lang3.StringUtils;
 
 // 메세지를 보내는 메소드를 모은 곳
 @Slf4j
@@ -12,8 +13,12 @@ public class JdaMsgSender {
 	// 메세지 보내는 원본 메소드
 	public static void send(TextChannel channel, String msg) {
 		log.info("메세지를 보내 보겠습니다. 현재 채널 메세지 발송 가능 여부: " + channel.canTalk());
-		if(channel.canTalk()) channel.sendMessage(msg).queue();
-		else log.info("메세지를 보낼 수가 없어요");
+		if(channel.canTalk()) {
+			msg = msgTrim(msg);
+			channel.sendMessage(msg).queue();
+		} else {
+			log.info("메세지를 보낼 수가 없어요");
+		}
 	}
 
 	// 특정 ID의 채널에 텍스트 채널에 메세지 전송
@@ -21,6 +26,7 @@ public class JdaMsgSender {
 		TextChannel channel = JdaController.instance.getTextChannelById(channelId);
 		log.info(channelId + " 채널에 메세지를 보냅니다. (채널 존재 여부: " + (channel != null) + ")");
 		if (channel != null) {
+			msg = msgTrim(msg);
 			send(channel, msg);
 		} else {
 			log.info("채널이 존재하지 않아 메세지를 보낼 수 없습니다.");
@@ -30,7 +36,28 @@ public class JdaMsgSender {
 	// 이벤트가 접수된 텍스트 채널에 메세지 전송. 모든 타입의 채널에 가능
 	public static void send(MessageReceivedEvent event, String msg) {
 		log.info("이벤트가 접수된 채널에 메세지를 보냅니다. (모든 타입의 채널 대응용 메소드 실행)");
+		msg = msgTrim(msg);
 		event.getChannel().sendMessage(msg).queue();
+	}
+
+	// Return trimmed msg.
+	// Check the msg length is over 2000.
+	// If then, trim it considering markdown backticks and add "...(생략)"
+	private static String msgTrim(String msg) {
+
+		if(StringUtils.isEmpty(msg) || msg.length() < 2000) return msg;
+
+		String last3Chars = msg.substring(msg.length() - 3);
+		boolean finishedWithCode = last3Chars.equals("```");
+		int cutLength = finishedWithCode ? 1997 : 2000;
+		cutLength -= 7; // "...(생략)" ← 이거 붙일려고..
+
+		String trimmedStr = msg.substring(0, cutLength);
+		if(finishedWithCode) trimmedStr += "```";
+		trimmedStr += "...(생략)";
+
+		return trimmedStr;
+
 	}
 
 }
