db894d4c12aa4e34c076167a243a45028193cde5
Junsung Park / Daanta <daanta@naver.com>
2023-01-16 01:34:47 +0900

    230116_0134 Props loading test

--- src/test/XLS_Reader_POI_Test/D230115_C01_xlsOpenAgain.java
+++ src/test/XLS_Reader_POI_Test/D230115_C01_xlsOpenAgain.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.util.CellUtil;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.Test;
@@ -16,8 +17,15 @@ import java.util.*;
 @Slf4j
 public class D230115_C01_xlsOpenAgain {
 
+    public static Map<String, Object> props = new HashMap<>();
+
+    private static Cell getStringFromAddr(XSSFSheet sheet, int rowIndex, int colIndex) {
+        Row row = CellUtil.getRow(rowIndex, sheet);
+        return CellUtil.getCell(row, colIndex);
+    }
+
     @Test
-    public void start() {
+    public void init() {
 
         String PATH = Paths.get("").toAbsolutePath().toString();
         log.info("현재 경로: " + PATH);
@@ -28,34 +36,47 @@ public class D230115_C01_xlsOpenAgain {
             // 파일 객체로부터 시트 객체 뽑아내기
             XSSFWorkbook workbook = new XSSFWorkbook(file)
             // try가 다 끝나면 위의 file과 workbook 객체에 대해 .close()가 실행되며 파일이 닫아진다.
+            // sheet 필드는 이 try 구문이 끝나면 소거되어 버리니, 처리할 것이 있으면 이 안에서 할 것.
         ) {
             // 최종적으로 sheet 객체를 얻는다.
             XSSFSheet sheet = workbook.getSheetAt(0);
-
-            // master props
-            Map<String, Object> props = new HashMap<>();
-
-            // Get Members
-            List<Map<String, String>> membersInfo = getMembersInfo(sheet);
-            props.put("members", membersInfo);
-            log.debug(props.get("members").toString());
-
+            log.debug("sheet loading success: {}", sheet);
+//            getMembersInfo(sheet);
+            getVersionInfo(sheet);
         } catch(Exception e) {
-            // log.error(ExceptionUtils.getStackTrace(e));
             e.printStackTrace();
+            log.debug("sheet loading failed");
         }
 
     }
 
-    public static List<Map<String, String>> getMembersInfo(XSSFSheet sheet) {
+    public void getVersionInfo(XSSFSheet sheet) {
+        props.put("version", getStringFromAddr(sheet, 2, 6));
+        props.put("build", getStringFromAddr(sheet, 3, 6));
+        props.put("cronSchedule", getStringFromAddr(sheet, 6, 6));
+        props.put("cronTargetChannelID", getStringFromAddr(sheet, 7, 6));
+        props.put("JDAToken", getStringFromAddr(sheet, 10, 6));
+        props.put("ChatGPTToken", getStringFromAddr(sheet, 11, 6));
+        StringBuilder sb = new StringBuilder();
+        for(int i = 12; i <= 23; i++) {
+            sb.append(getStringFromAddr(sheet, i, 6));
+            if(i != 23) sb.append("\n");
+        }
+        props.put("GoogleCloudToken", sb.toString());
+        log.debug("FINISHED PROPS LOADING! {}", props);
+    }
 
-        // Go third row
+    public static void getMembersInfo(XSSFSheet sheet) {
+
+        // Result
+        List<Map<String, String>> membersInfo = new ArrayList<>();
+
+        // Go to 3rd row
         Iterator<Row> rowIterator = sheet.iterator();
         rowIterator.next();
         rowIterator.next();
 
         // Read the member list starting 4th row (address '?4' ~ )
-        List<Map<String, String>> membersInfo = new ArrayList<>();
         while(rowIterator.hasNext()) {
 
             // Target values
@@ -69,10 +90,11 @@ public class D230115_C01_xlsOpenAgain {
             // Go 2nd column: B cell, member name
             // REQUIRED
             Cell cellName = cellIterator.next(); // 2nd cell (Address 'B?')
-            // If meets a blank in B cell, don't read this row anymore and go to next row
+            // If meets a blank in B cell, don't read members' info anymore and stop reading
             if(cellName.getCellType() == CellType.BLANK) {
                 log.info("No member found in ({})", cellName.getAddress());
-                continue;
+                log.info("FINISHED MEMBERS LOADING!");
+                break;
             }
             name = cellName.getStringCellValue();
             member.put("name", name);
@@ -104,7 +126,7 @@ public class D230115_C01_xlsOpenAgain {
 
         }
 
-        return membersInfo;
+        props.put("members", membersInfo);
 
     }
 }
